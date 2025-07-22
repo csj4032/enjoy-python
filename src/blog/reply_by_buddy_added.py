910da3d101f7fee7851809674c19a08544e34359
genius.choi <csj4032@gmail.com>
2025-07-22 14:11:07 +0900

    refactor: remove unused parse_buddy_by_added function and clean up imports in reply_by_buddy_added

--- src/blog/reply_by_buddy_added.py
+++ src/blog/reply_by_buddy_added.py
@@ -8,7 +8,7 @@ from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
-from common.webs import setup_firefox_profile_driver, parse_buddy_by_added, move_to_buddy_added_scroll, try_click_element, process_reply_and_is_limited, get_buddies_by_added, get_buddies_by_added_with
+from common.webs import setup_firefox_profile_driver, move_to_buddy_added_scroll, try_click_element, process_reply_and_is_limited, get_buddies_by_added_with
 from config.configuration import Configuration
 
 __prompt = "'{0}' 이라는 제목의 블로그 글에 대한 코멘트를 하나만 간단하게 작성해줘. 아래 '{1}' 내용을 참고해서, 방문자 입장에서 담백하고 자연스럽게 작성해야 해. 코멘트는 50자 내외로 해줘"
