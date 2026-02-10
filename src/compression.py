2fe0facbd9a1d63a55d8f81c0082b712708d4b86
genius.choi <csj4032@gmail.com>
2026-02-10 11:00:22 +0900

    chore(compression): remove unused pandas import and clean up code

--- src/compression.py
+++ src/compression.py
@@ -1,6 +1,5 @@
 import json
 
-import pandas as pd
 from faker import Faker
 
 
@@ -17,10 +16,8 @@ def generate_fake_data() -> list[dict[str, str]]:
         "sentence": faker.sentence()
     }) for _ in range(1)]
 
+
 if __name__ == '__main__':
     fake_data = generate_fake_data()
     fake_data_json = json.dumps(fake_data, ensure_ascii=False)
     with open("./fake_data.json", "w") as f: f.write(fake_data_json)
-
-    df = pd.read_parquet("/Users/genius/Workspace/enjoy-docker/stream/warehouse/default_database/genius_departments_iceberg_sink/data/00000-0-b31a2bae-b447-4a0f-85e2-9d91cf18a61f-00001.parquet")
-    print(df.head())
